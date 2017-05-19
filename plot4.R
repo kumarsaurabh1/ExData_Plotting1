956163400fce3940702759132dbccd8c9ce43cb3
kumarsaurabh1 <kumar.saurabh1@tcs.com>
2017-05-19 18:23:09 +0530

    Exploratory Data Analysis
    Exploratory Data Analysis

--- plot4.R
+++ plot4.R
@@ -1,4 +1,5 @@
 #set working directory in which this power cosumption file is placed
+
 dataFile <- "household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
